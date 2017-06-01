8c2d12bb3289ecf005e4fdca8e5873e464758cf9
aledelpesce <ek12projekt03>
2017-06-01 14:39:53 +0200

    Fix drawer color

--- app/src/main/java/it/eng/moband2017/DettaglioActivity.java
+++ app/src/main/java/it/eng/moband2017/DettaglioActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
